d4cae8d07d8c87745ad52961e978564031ec5c59
Kieran Cutting <kierancutting@gmail.com>
2022-11-30 16:08:57 +0000

    Restore sendgrid

--- medusa-config.js
+++ medusa-config.js
@@ -62,21 +62,20 @@ const plugins = [
       api_key: STRIPE_API_KEY,
       webhook_secret: STRIPE_WEBHOOK_SECRET,
     },
-  },
-  //  {
-  //   resolve: `medusa-plugin-sendgrid`,
-  //   options: {
-  //     api_key: process.env.SENDGRID_API_KEY,
-  //     from: process.env.SENDGRID_FROM,
-  //     order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
-  //     user_password_reset_template: process.env.SENDGRID_USER_PASSWORD_RESET_ID,
-  //     customer_password_reset_template: process.env.SENDGRID_CUSTOMER_PASSWORD_RESET_ID,
-  //     order_shipped_template: process.env.SENDGRID_ORDER_SHIPPED_ID,
-  //     order_canceled_template: process.env.SENDGRID_ORDER_CANCELED_ID,
-  //     // _template: process.env.SENDGRID_ID,
+  }, {
+    resolve: `medusa-plugin-sendgrid`,
+    options: {
+      api_key: process.env.SENDGRID_API_KEY,
+      from: process.env.SENDGRID_FROM,
+      order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+      user_password_reset_template: process.env.SENDGRID_USER_PASSWORD_RESET_ID,
+      customer_password_reset_template: process.env.SENDGRID_CUSTOMER_PASSWORD_RESET_ID,
+      order_shipped_template: process.env.SENDGRID_ORDER_SHIPPED_ID,
+      order_canceled_template: process.env.SENDGRID_ORDER_CANCELED_ID,
+      // _template: process.env.SENDGRID_ID,
 
-  //   }
-  // }
+    }
+  }
 ];
 
 module.exports = {
